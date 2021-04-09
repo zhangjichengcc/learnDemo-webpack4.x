fb59b7f2ad21a68ffc0938ef64bd28f74a0c6a19
zhangjicheng <zhangjichengcc@163.com>
2021-04-09 19:09:15 +0800

    update: add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 /*
  * @Author: zhangjicheng
  * @Date: 2021-04-08 14:21:04
- * @LastEditTime: 2021-04-08 16:07:01
+ * @LastEditTime: 2021-04-09 18:58:53
  * @LastEditors: zhangjicheng
  * @Description: 
  * @FilePath: \learnDemo-webpack4.0\webpack.config.js
  * 可以输入预定的版权声明、个性签名、空行等
  */
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 
 
 module.exports = {
   mode: 'development', // 指定构建模式 development 为未压缩版
@@ -18,6 +19,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), // 指定构建生成文件所在路径
     filename: 'bundle[hash].js', // 指定构建生成的文件名
   },
+
+  plugins: [
+    // new HtmlWebpackPlugin(), // 默认在dist 创建.html并引入 js
+    new HtmlWebpackPlugin({
+      template: 'src/index.html', // 配置文件模板
+    }),
+  ],
 
   devServer: {
     contentBase: path.resolve(__dirname, 'dist') // 开发服务器启动路径
